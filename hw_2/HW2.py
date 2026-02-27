c5bf48919cacf5fb68a216cc9ac9b8dca5ee3ff4
hardikkamboj <hardikkamboj1@gmail.com>
2026-02-26 23:28:29 -0500

    added prefix notation

--- hw_2/HW2.py
+++ hw_2/HW2.py
@@ -44,7 +44,15 @@ class HomeWork2:
     # you can see the examples in p2_traversals.csv
 
     def prefixNotationPrint(self, head: TreeNode) -> list:
-        pass
+        result = []
+        def preorder(node):
+            if node is None:
+                return
+            result.append(node.val)
+            preorder(node.left)
+            preorder(node.right)
+        preorder(head)
+        return result
 
     # Problem 2.2: Use in-order traversal (left, root, right) for infix notation with appropriate parentheses.
     # return an array of elements of an infix expression
